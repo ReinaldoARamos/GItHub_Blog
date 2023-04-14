1af9269792a9b4043f481a5454ea5d531bf65c5b
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-14 14:28:52 -0300

    terminando a tipografia de fato

--- src/Typography/Typography.ts
+++ src/Typography/Typography.ts
@@ -13,6 +13,17 @@ interface RegularTextProps{
 
 
 export const TitleText = styled.h1<TitleTextProps>`
-    color: ${({theme, color}) => theme.colors[`base-${color ?? "title"}`]}
+    color: ${({theme, color}) => theme.colors[`base-${color ?? "title"}`]};
+    font-size: ${({theme, size}) => theme.textSizes[`title-title-${size ?? "l"}`]};
+    font-family: ${({theme}) => theme.fonts.title};
+    line-height: 160%;
+    font-weight: bold;
+` 
+
+export const Regular = styled.p<RegularTextProps>`
+    color: ${({theme, color}) => theme.colors[`base-${color ?? "text"}`]};
+    font-size: ${({theme, size}) => theme.textSizes[`text-regular-${size ?? "l"}`]};
+    font-family: ${({theme}) => theme.fonts.title};
+    line-height: 160%;
 
 ` 
