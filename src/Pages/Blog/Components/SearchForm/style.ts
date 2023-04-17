f7449b550d58ecd5ee7a8751b20b07329d06a2d6
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-17 15:42:21 -0300

    criando o container onde terá o input

--- src/Pages/Blog/Components/SearchForm/style.ts
+++ src/Pages/Blog/Components/SearchForm/style.ts
@@ -1,9 +1,15 @@
-import styled  from "styled-components";
-
+import styled from "styled-components";
 
 export const SearchContainer = styled.div`
+  margin-top: 72px;
 
-margin-top: 72px;
+  background-color: red;
+  width: 864px;
+  height: 70px;
+  margin-left: 288px;
+  padding: 12px, 16px, 12px, 16px;
 
-background-color: red;
-` 
+  display: flex;
+
+  justify-content: center;
+`;
