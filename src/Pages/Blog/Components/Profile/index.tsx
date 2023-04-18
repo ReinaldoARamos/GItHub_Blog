e25e29f4e6f9dd265c7bc69bbb573dd3815874b8
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-04-18 15:13:03 -0300

    integrando a API do github com axios

--- src/Pages/Blog/Components/Profile/index.tsx
+++ src/Pages/Blog/Components/Profile/index.tsx
@@ -7,23 +7,38 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import ProfilePic from '../../../../assets/profileIcon.jfif'
 import { NavLink } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../../../../lib/axios";
+//const username = import.meta.env.VITE_GITHUB_USERNAME;
 
 
 interface User {
-    username: String,
+  login: String,
     followers: String,
     company: String
 } 
 export function Profile() {
 
+  const [profileData, setProfileData] = useState<User>({} as User)
 
+  const getProfileData = useCallback(async () => {
+    const response =  await api.get('users/ReinaldoARamos')
+
+    setProfileData(response.data)
+
+    console.log(response.data)
+  }, [profileData])
+
+  useEffect(() =>{
+    getProfileData();
+  }, [])
 
   return (
     <ProfileContainer>
       <img src={ProfilePic} />
       <ProfileContent>
         <header>
-          <h1>Reinaldo Ramos</h1>
+          <h1>{profileData.login}</h1>
          
             <NavLink  to={"/post"}>
             <ExternalLinks text={"github"} />
--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -3,5 +3,5 @@ import axios from "axios";
 
 
 export const api = axios.create({
-    baseURL: 'https://api.github.com/users'
+    baseURL: 'https://api.github.com'
 })
